af155bb68a1064c771a1d5dfeb61f4f095c65f93
Juan Mendez <kozincmusic@gmail.com>
2021-12-13 16:11:06 -0500

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Main from './components/MainComponent';
 
-//To disable the yellow warnings on the Android Emulator, uncomment the line below.
-//console.disableYellowBox = true;
-
 export default function App() {
     return (
         <Main />
