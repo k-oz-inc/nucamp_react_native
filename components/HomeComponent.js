8ba0a1d3053cdc4ab90d58564b2c3f1828eec270
Juan Mendez <kozincmusic@gmail.com>
2021-12-29 22:48:29 -0500

    Animatable

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Animated } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
