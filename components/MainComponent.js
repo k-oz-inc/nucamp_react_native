fc7ec15dde8ee61b5ccf98885684d93e85a8802e
Juan Mendez <kozincmusic@gmail.com>
2022-01-09 15:41:48 -0500

    React Native Week 4 Workshop

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -330,13 +330,7 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromotions();
     this.props.fetchPartners();
-
-    NetInfo.fetch().then(connectionInfo => {
-      (Platform.OS === 'ios')
-        ? Alert.alert('Initial Network Conectivity Type:', connectionInfo.type)
-        : ToastAndroid.show('Initial Network Conectivity Type: ' +
-          connectionInfo.type, ToastAndroid.LONG); 
-    });
+    this.showNetInfo();
 
     this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
       this.handleConnectivityChange(connectionInfo);
@@ -367,6 +361,17 @@ class Main extends Component {
       ? Alert.alert('Connection change:', connectionMsg) 
       : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
   }
+
+  showNetInfo = async () => {
+    const connectionInfo = await fetch(
+      Platform.OS === "ios"
+        ? Alert.alert("Initial Network Connectivity Type:", connection.type)
+        : ToastAndroid.show(
+            "Initial Network Connectivity Type: " + connectionInfo.type,
+            ToastAndroid.LONG
+          )
+    );
+  };
 
   render() {
     return (
